e1c43b6d2a41c1f26c29134459de0f966637c163
Shashi Ranjan <82989977+739ranjan@users.noreply.github.com>
2024-01-29 18:32:33 +0530

    Create IntersectionOfTwoSortedLinkedList.java
    Given two linked lists sorted in increasing order, create a new linked list representing the intersection of the two linked lists. The new linked list should be made with without changing the original lists.
    
    Note: The elements of the linked list are not necessarily distinct.
    
    Example 1:
    
    Input:
    LinkedList1 = 1->2->3->4->6
    LinkedList2 = 2->4->6->8
    Output: 2 4 6
    Explanation: For the given two
    linked list, 2, 4 and 6 are the elements
    in the intersection.
    Example 2:
    
    Input:
    LinkedList1 = 10->20->40->50
    LinkedList2 = 15->40
    Output: 40
    Your Task:
    You don't have to take any input of print anything. Your task is to complete the function findIntersection(), which will take head of both of the linked lists as input and should find the intersection of two linked list and add all the elements in intersection to the third linked list and return the head of the third linked list.
    
    Expected Time Complexity : O(n+m)
    Expected Auxilliary Space : O(n+m)
    Note: n, m are the size of the respective linked lists.
    
    Constraints:
    1 <= size of linked lists <= 5000
    1 <= Data in linked list nodes <= 104

---
+++ IntersectionOfTwoSortedLinkedList.java
@@ -0,0 +1,108 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+class Node 
+{
+    int data;
+    Node next;
+    
+    Node(int a)
+        {
+            data = a;
+            next = null;
+        }
+}
+class IntersectionOfTwoSortedLinkedList
+{
+    static Scanner sc = new Scanner(System.in);
+    public static Node inputList(int size)
+    {
+        Node head, tail;
+        int val;
+        
+        val = sc.nextInt();
+        
+        head = tail = new Node(val);
+        
+        size--;
+        
+        while(size-->0)
+        {
+            val = sc.nextInt();
+            tail.next = new Node(val);
+            tail = tail.next;
+        }
+        
+        return head;
+    }
+    
+    public static void printList(Node n)
+    {
+        while(n!=null)
+        {
+            System.out.print(n.data + " ");
+            n = n.next;
+        }
+    }
+    
+    public static void main(String args[])
+        {
+            
+            int t = sc.nextInt();
+            while(t-->0)
+                {
+                    int n , m;
+                    
+                    n = sc.nextInt();
+                    m = sc.nextInt();
+                    
+                    Node head1 = inputList(n);
+                    Node head2 = inputList(m);
+                    
+                    Solution obj = new Solution();
+                    
+                    Node result = obj.findIntersection(head1, head2);
+	    
+	                printList(result);
+	                System.out.println();
+                }
+        }
+}
+// } Driver Code Ends
+
+
+/* Node of a linked list
+ class Node {
+   int data;
+    Node next;
+    Node(int d)  { data = d;  next = null; }
+}
+*/
+
+class Solution
+{
+   public static Node findIntersection(Node head1, Node head2)
+    {
+        if(head1 == null || head2 == null)
+            return null;
+        
+        Node res = new Node(-1);
+        Node curr = res;
+        Node list1 = head1, list2 = head2;
+        
+        while(list1 != null && list2 != null){
+            if(list1.data == list2.data){
+                curr.next = list1;
+                list1 = list1.next;
+                list2 = list2.next;
+                curr = curr.next;
+                curr.next = null;
+            }else if(list1.data > list2.data){
+                list2 = list2.next;
+            }else{
+                list1 = list1.next;
+            }
+        }
+        return res.next;
+    }
+}
